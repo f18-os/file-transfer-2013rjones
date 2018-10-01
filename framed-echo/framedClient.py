ba44a02f50440c84a5f41025b31ba74b41849d02
Ryan Jones(VM) <2013rjones@gmail.com>
2018-10-01 01:22:52 -0600

    Modified code to fix import error and added command bar and selective port to connect to.

--- framed-echo/framedClient.py
+++ framed-echo/framedClient.py
@@ -1,10 +1,10 @@
 #! /usr/bin/env python3
 
-# Echo client program
-import socket, sys, re
+import sys
 
 sys.path.append("../lib")       # for params
-import params
+
+import re, socket, params,os
 
 from framedSock import framedSend, framedReceive
 
@@ -21,6 +21,11 @@ paramMap = params.parseParams(switchesVarDefaults)
 
 server, usage, debug  = paramMap["server"], paramMap["usage"], paramMap["debug"]
 
+serverParse = str(input("Please input server socket you wish to connect to:"))
+
+if(serverParse.strip() != ""): 
+    server = "127.0.0.1:" + str(serverParse).strip()
+
 if usage:
     params.usage()
 
@@ -35,6 +40,7 @@ except:
 s = None
 for res in socket.getaddrinfo(serverHost, serverPort, socket.AF_UNSPEC, socket.SOCK_STREAM):
     af, socktype, proto, canonname, sa = res
+    
     try:
         print("creating sock: af=%d, type=%d, proto=%d" % (af, socktype, proto))
         s = socket.socket(af, socktype, proto)
@@ -56,8 +62,33 @@ if s is None:
     print('could not open socket')
     sys.exit(1)
 
+cont = True 
+while(cont):
+    print(" ")
+    print("Please enter:")
+    print("             0 to send a file to a server")
+    print("             1 to display files on server")
+    print("             2 to exit")
+    try:
+        cmd = input("Please input number corresponding to command.")
+        if(str(cmd).strip() != ""):
+            try: 
+                if(cmd == 0): 
+                    print("Sending a file to a server.")
+                    print("Please input file to transfer")
+                if(cmd == "1"): 
+                    print("Displaying all files on a server. ")
+                if(cmd == 2): 
+                    cont = False
+                if((cmd != 0) and (cmd != 1) and (cmd != 2)): 
+                    print("Please enter 0, 1, or 2 ")
+            except:
+                print("Invalid input, please enter a number as a command.")
 
-print("sending hello world")
+    except: 
+        print("No number input, please try again. ")    
+        
+   
 framedSend(s, b"hello world", debug)
 print("received:", framedReceive(s, debug))
 
