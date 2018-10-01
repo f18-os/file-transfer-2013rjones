14ab9a17af0f0130e843ca14fed5baebd8538c96
Ryan Jones(VM) <2013rjones@gmail.com>
2018-10-01 13:26:51 -0600

    added functionality to write a file taken from a client.

---
+++ file-transfer-lab/framedServer.py
@@ -0,0 +1,74 @@
+#! /usr/bin/env python3
+import sys
+sys.path.append("../lib")       # for params
+import re, socket, params
+
+
+switchesVarDefaults = (
+    (('-l', '--listenPort') ,'listenPort', 50001),
+    (('-d', '--debug'), "debug", False), # boolean (set if present)
+    (('-?', '--usage'), "usage", False), # boolean (set if present)
+    )
+
+progname = "echoserver"
+paramMap = params.parseParams(switchesVarDefaults)
+
+debug, listenPort = paramMap['debug'], paramMap['listenPort']
+
+if paramMap['usage']:
+    params.usage()
+
+lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # listener socket
+bindAddr = ("127.0.0.1", listenPort)
+lsock.bind(bindAddr)
+lsock.listen(5)
+print("listening on:", bindAddr)
+
+while True:
+    sock, addr = lsock.accept()
+
+    print("connection rec'd from", addr)
+
+
+    from framedSock import framedSend, framedReceive
+    notStarted = True 
+    openFile = False 
+    fileGiven = ""
+    cmdEntered = False
+    while True:
+        payload = framedReceive(sock, debug)
+        print("read: " + payload)
+        cmds = payload.split()
+        if(notStarted): 
+            for cmd in cmds: 
+                if(openFile): 
+                    fileGiven = open(cmd+"ServerVers", 'w') #open file in server Area appending ServerVers
+                    notStarted = False 
+                if(cmd == "-StrFl"):
+                    #then we have a file to open. 
+                    openFile = True 
+                
+        
+        
+        else: 
+            for cmd in cmds:
+                if(cmdEntered): 
+                    if(cmd == "CloseFileWritingChunks"):
+                        fileGiven.close()
+                        cmdEntered = False 
+                        notStarted = True
+                if(cmd == "-cmd"): 
+                    #then next will be to close 
+                    cmdEntered = True
+                else:
+                    break 
+            
+            #then we write a line if we get here. 
+            if(notStarted == False): 
+                fileGiven.write(payload + '\n') #modify this to take the parts of a same line.  
+        if debug: print("rec'd: ", payload)
+        
+        if not payload:
+            break
+        payload += b"!"             # make emphatic!
+        framedSend(sock, payload, debug)
